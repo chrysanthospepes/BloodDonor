8c1873843f7debcf3793a86d8e11f12eb581b390
chrysanthospepes <pepesxrisanthos@gmail.com>
2024-01-06 14:33:57 +0200

    Initial commit for project

---
+++ src/main/java/ds/course/group/fiftyone/blooddonor/NationalBloodDonorRegistryApplication.java
@@ -0,0 +1,13 @@
+package ds.course.group.fiftyone.blooddonor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalBloodDonorRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalBloodDonorRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ds/course/group/fiftyone/blooddonor/NationalBloodDonorRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ds.course.group.fiftyone.blooddonor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalBloodDonorRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
