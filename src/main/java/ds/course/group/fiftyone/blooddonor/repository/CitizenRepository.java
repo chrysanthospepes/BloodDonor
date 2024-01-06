1e85b210d8bf90cf1fd5f51b15205439a46f52d3
chrysanthospepes <pepesxrisanthos@gmail.com>
2024-01-06 16:53:40 +0200

    feat: repositories for citizen and bloodType

---
+++ src/main/java/ds/course/group/fiftyone/blooddonor/repository/CitizenRepository.java
@@ -0,0 +1,9 @@
+package ds.course.group.fiftyone.blooddonor.repository;
+
+import ds.course.group.fiftyone.blooddonor.entity.Citizen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CitizenRepository extends JpaRepository<Citizen, Long> {`
+}
