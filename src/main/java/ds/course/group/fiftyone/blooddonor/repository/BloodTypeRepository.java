e0ea0f86408670eb3b1a26c47521d39878cd5122
chrysanthospepes <pepesxrisanthos@gmail.com>
2024-01-06 23:01:04 +0200

    fix: repository tag

--- src/main/java/ds/course/group/fiftyone/blooddonor/repository/BloodTypeRepository.java
+++ src/main/java/ds/course/group/fiftyone/blooddonor/repository/BloodTypeRepository.java
@@ -3,9 +3,10 @@ package ds.course.group.fiftyone.blooddonor.repository;
 import ds.course.group.fiftyone.blooddonor.entity.BloodType;
 import ds.course.group.fiftyone.blooddonor.entity.BloodTypes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(path= "bloodtype")
 public interface BloodTypeRepository extends JpaRepository<BloodType, Long> {
     boolean findByBloodType(BloodTypes bloodType);
 }
--- src/main/java/ds/course/group/fiftyone/blooddonor/repository/CitizenRepository.java
+++ src/main/java/ds/course/group/fiftyone/blooddonor/repository/CitizenRepository.java
@@ -2,9 +2,9 @@ package ds.course.group.fiftyone.blooddonor.repository;
 
 import ds.course.group.fiftyone.blooddonor.entity.Citizen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(path= "citizen")
 public interface CitizenRepository extends JpaRepository<Citizen, Long> {
-    Citizen saveCitizen(Citizen citizen);
 }
