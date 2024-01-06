25f34f22e277ba6805c89f98993612953d59cc62
chrysanthospepes <pepesxrisanthos@gmail.com>
2024-01-06 21:51:22 +0200

    feat: security

---
+++ src/main/java/ds/course/group/fiftyone/blooddonor/controller/AuthController.java
@@ -0,0 +1,145 @@
+package ds.course.group.fiftyone.blooddonor.controller;
+
+import ds.course.group.fiftyone.blooddonor.payload.request.*;
+import ds.course.group.fiftyone.blooddonor.config.JwtUtils;
+import ds.course.group.fiftyone.blooddonor.entity.Role;
+import ds.course.group.fiftyone.blooddonor.entity.User;
+import ds.course.group.fiftyone.blooddonor.payload.response.*;
+import ds.course.group.fiftyone.blooddonor.repository.RoleRepository;
+import ds.course.group.fiftyone.blooddonor.repository.UserRepository;
+import ds.course.group.fiftyone.blooddonor.service.UserDetailsImpl;
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    BCryptPasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+
+    @PostConstruct
+    public void setup() {
+        roleRepository.findByName("ROLE_ADMIN").orElseGet(() -> {
+            roleRepository.save(new Role("ROLE_ADMIN"));
+            return null;
+        });
+        roleRepository.findByName("ROLE_USER").orElseGet(() -> {
+            roleRepository.save(new Role("ROLE_USER"));
+            return null;
+        });
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        System.out.println("authentication");
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        System.out.println("authentication: " + authentication);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        System.out.println("post authentication");
+        String jwt = jwtUtils.generateJwtToken(authentication);
+        System.out.println("jw: " + jwt);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName("ROLE_USER")
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName("ROLE_ADMIN")
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "mod":
+                        Role modRole = roleRepository.findByName("ROLE_MODERATOR")
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName("ROLE_USER")
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+
+
+}
