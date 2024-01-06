655419d5fd658bb39a2f988fb9bc55e16928e409
chrysanthospepes <pepesxrisanthos@gmail.com>
2024-01-06 22:49:14 +0200

    add: secured roles

--- src/main/java/ds/course/group/fiftyone/blooddonor/api/CitizenRestController.java
+++ src/main/java/ds/course/group/fiftyone/blooddonor/api/CitizenRestController.java
@@ -3,6 +3,7 @@ package ds.course.group.fiftyone.blooddonor.api;
 import ds.course.group.fiftyone.blooddonor.entity.Citizen;
 import ds.course.group.fiftyone.blooddonor.service.CitizenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,21 +15,25 @@ public class CitizenRestController {
     @Autowired
     private CitizenService citizenService;
 
+//    @Secured({"ROLE_ADMIN", "ROLE_MODERATOR"})
     @GetMapping("")
     public List<Citizen> showCitizens() {
         return citizenService.getCitizens();
     }
 
+//    @Secured({"ROLE_ADMIN", "ROLE_MODERATOR"})
     @PostMapping("")
     public Citizen saveCitizen(@RequestBody Citizen citizen){
         return citizenService.saveCitizen(citizen);
     }
 
+//    @Secured({"ROLE_ADMIN", "ROLE_MODERATOR"})
     @GetMapping("/{id}")
     public Citizen getCitizen(@PathVariable("id") Long id) {
         return citizenService.getCitizen(id);
     }
 
+//    @Secured({"ROLE_ADMIN", "ROLE_MODERATOR"})
     @DeleteMapping("/{id}")
     public void deleteCitizen(@PathVariable("id") Long id) {
         citizenService.deleteCitizen(id);
