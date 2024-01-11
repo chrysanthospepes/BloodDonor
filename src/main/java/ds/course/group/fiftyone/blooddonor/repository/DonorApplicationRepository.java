a9fd703d0cad2312b51290ba662e3ae9df3d93a2
chrysanthospepes <pepesxrisanthos@gmail.com>
2024-01-11 18:32:34 +0200

    add: find unreviewed forms

--- src/main/java/ds/course/group/fiftyone/blooddonor/repository/DonorApplicationRepository.java
+++ src/main/java/ds/course/group/fiftyone/blooddonor/repository/DonorApplicationRepository.java
@@ -4,6 +4,9 @@ import ds.course.group.fiftyone.blooddonor.entity.DonorApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(path= "donorapplication")
 public interface DonorApplicationRepository extends JpaRepository<DonorApplication, Long> {
+    List<DonorApplication> findAllByIsReviewedFalse();
 }
